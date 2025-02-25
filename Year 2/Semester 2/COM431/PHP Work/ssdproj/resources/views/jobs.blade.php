4d100f493f69e09b0e1f8fda0c2fffd27d516f13
andrewmcc03 <126252984+andrewmcc03@users.noreply.github.com>
2025-02-25 22:52:16 +0000

    Week 5 (PhpStorm Commit 4)

--- Year 2/Semester 2/COM431/PHP Work/ssdproj/resources/views/jobs.blade.php
+++ Year 2/Semester 2/COM431/PHP Work/ssdproj/resources/views/jobs.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
     <x-slot:heading>Job Listings Page</x-slot:heading>
-    
-    <ul>
+
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 test-sm">{{ $job->employer->name }}</div>
+                <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }}/year.
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
