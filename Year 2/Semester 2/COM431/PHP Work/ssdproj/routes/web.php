8006076f8ec022b4d34238b1820b2a4c933b4d8d
andrewmcc03 <126252984+andrewmcc03@users.noreply.github.com>
2025-04-01 18:55:23 +0100

    Week 10 (PhpStorm commit 2)

--- Year 2/Semester 2/COM431/PHP Work/ssdproj/routes/web.php
+++ Year 2/Semester 2/COM431/PHP Work/ssdproj/routes/web.php
@@ -9,12 +9,25 @@ Route::view('/', 'home', ['greeting' => 'Hello', 'name' => 'Andrew']);
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class);
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])
+    ->middleware('auth');
+
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+
+//Route::resource('jobs', JobController::class)->only(['index', 'show'])->middleware('auth');
 
 // Auth
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
